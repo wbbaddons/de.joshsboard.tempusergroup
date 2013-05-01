0372223e252541e820b57482fe27490a5ef7d178
joshuarüsweg <ruesweg@intechmedia.de>
2013-05-01 10:01:37 +0200

    first push

---
+++ files/lib/system/cronjob/RemoveUserFromGroupCronjob.class.php
@@ -0,0 +1,32 @@
+<?php
+namespace wcf\system\cronjob;
+use wcf\data\cronjob\Cronjob;
+use wcf\system\WCF;
+use wcf\data\user\UserEditor;
+
+class RemoveUserFromGoupCronjob extends AbstractCronjob {
+	/**
+	 * @see	wcf\system\cronjob\ICronjob::execute()
+	 */
+	public function execute(Cronjob $cronjob) {
+		parent::execute($cronjob);
+		
+		$sql = "SELECT	userID, groupID
+			FROM	wcf".WCF_N."_user_to_group_temp
+			WHERE until < ".TIME_NOW;
+		$statement = WCF::getDB()->prepareStatement($sql);
+		$statement->execute();
+
+		while ($row = $statement->fetchArray()) {
+			$user = new UserEditor($row['userID']);
+			$user->removeFromGroups(array($row['groupID']));
+			$user->resetCache();
+                        
+                        $sql = "DELETE FROM wcf".WCF_N."_user_to_group_temp
+                            WHERE 
+                            userID = ? AND groupID = ?";
+                        $statement = WCF::getDB()->prepareStatement($sql);
+                        $statement->execute(array($row['userID'], $row['groupID']));
+		}
+	}
+}
---
+++ install.sql
@@ -0,0 +1,5 @@
+CREATE TABLE wcf1_user_to_group_temp (
+userID INT NOT NULL ,
+groupID INT NOT NULL ,
+until INT( 10 ) NOT NULL
+) ENGINE = INNODB;
